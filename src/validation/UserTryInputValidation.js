a2fbca1cf5b3412a322a78e0ab08ab91e3979f43
sh981013s <sh981013s@gmail.com>
2022-12-16 02:44:13 +0900

    feat(UserTryInputValidation): 유저 숫자 입력 validation 로직 구현

---
+++ src/validation/UserTryInputValidation.js
@@ -0,0 +1,26 @@
+const USER_TRY_MESSAGES = require("../constants/Messages");
+
+const UserTryInputValidation = {
+  hasDifferentThreeValues(splitUserInput) {
+    return new Set(splitUserInput).size === 3;
+  },
+
+  isNumberInRange(splitUserInput) {
+    for (const singleNumber of splitUserInput) {
+      if (1 > splitUserInput || splitUserInput > 9) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  validate(userInput) {
+    const splitUserInput = userInput.split("").map(Number);
+    if (
+      !this.hasDifferentThreeValues(splitUserInput) ||
+      !this.isNumberInRange(splitUserInput)
+    ) {
+      throw new Error(USER_TRY_MESSAGES.ERROR);
+    }
+  },
+};
