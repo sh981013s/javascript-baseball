6c87607ad68468576c6e5a4954ae5bf1aeda521e
sh981013s <sh981013s@gmail.com>
2022-12-15 21:26:00 +0900

    feat(Messages): 유저 시도 input 관련 메세지 작성

---
+++ src/constants/Messages.js
@@ -0,0 +1,7 @@
+const USER_TRY_MESSAGES = Object.freeze({
+  OPENING: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  ERROR: "서로 다른 3자리의 수를 입력해주셔야 합니다.",
+});
+
+module.exports = USER_TRY_MESSAGES;
