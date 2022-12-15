258e4cc97301f38cd940bfab6623535e8f8578aa
sh981013s <sh981013s@gmail.com>
2022-12-15 21:37:01 +0900

    feat(GeneralConstants): 숫자 범위 상수화

---
+++ src/constants/GeneralConstants.js
@@ -0,0 +1,11 @@
+const GENERAL_CONSTANTS = Object.freeze({
+  START_NUMBER: 1,
+  END_NUMBER: 9,
+});
+
+const REGEX_EXPRESSION = Object.freeze({});
+
+module.exports = {
+  GENERAL_CONSTANTS,
+  REGEX_EXPRESSION,
+};
