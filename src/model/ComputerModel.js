73e78d31bec6817a33029bd9abbbcfa9ff236975
sh981013s <sh981013s@gmail.com>
2022-12-15 21:49:52 +0900

    feat(ComputerModel): 랜덤 숫자 생성 로직 구현

--- src/model/ComputerModel.js
+++ src/model/ComputerModel.js
@@ -1,3 +1,6 @@
+const { Random } = require("@woowacourse/mission-utils");
+const { GENERAL_CONSTANTS } = require("../constants/GeneralConstants");
+
 class ComputerModel {
   #computerNumber;
 
@@ -7,6 +10,26 @@ class ComputerModel {
 
   setComputerNumber(newComputerNumber) {
     this.#computerNumber = newComputerNumber;
+  }
+
+  getGeneratedSingleNumber() {
+    const singleNumber = Random.pickNumberInRange(
+      GENERAL_CONSTANTS.START_NUMBER,
+      GENERAL_CONSTANTS.END_NUMBER,
+    );
+
+    return singleNumber;
+  }
+
+  generateComputerNumber() {
+    const generatedNumber = [];
+    while (new Set(generatedNumber).size !== 3) {
+      const singleNumber = this.getGeneratedSingleNumber();
+      if (!generatedNumber.includes(singleNumber)) {
+        generatedNumber.push(singleNumber);
+      }
+    }
+    return generatedNumber;
   }
 }
 
