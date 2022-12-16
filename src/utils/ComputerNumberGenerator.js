1c7f2e521240ff0f9cc2b694bd88af6fa547ce15
sh981013s <sh981013s@gmail.com>
2022-12-16 10:59:22 +0900

    feat(ComputerNumberGenerator): 타겟 숫자 생성 로직 분리 구현

---
+++ src/utils/ComputerNumberGenerator.js
@@ -0,0 +1,26 @@
+const { Random } = require("@woowacourse/mission-utils");
+const { GENERAL_CONSTANTS } = require("../constants/GeneralConstants");
+
+class ComputerNumberGenerator {
+  #getGeneratedSingleNumber() {
+    const singleNumber = Random.pickNumberInRange(
+      GENERAL_CONSTANTS.START_NUMBER,
+      GENERAL_CONSTANTS.END_NUMBER,
+    );
+
+    return singleNumber;
+  }
+
+  getGeneratedComputerNumber() {
+    const generatedNumber = [];
+    while (new Set(generatedNumber).size !== 3) {
+      const singleNumber = this.#getGeneratedSingleNumber();
+      if (!generatedNumber.includes(singleNumber)) {
+        generatedNumber.push(singleNumber);
+      }
+    }
+    return generatedNumber;
+  }
+}
+
+module.exports = ComputerNumberGenerator;
