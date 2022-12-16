580b80f4aee9c1c06268ca06c875e0d21336b788
sh981013s <sh981013s@gmail.com>
2022-12-16 10:52:53 +0900

    test(ComputerTest): 타겟 숫자 생성 로직 분리에 따른 테스트코드 작성

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -2,8 +2,8 @@ const ComputerNumberGenerator = require("../src/utils/ComputerNumberGenerator");
 
 describe("컴퓨터 숫자 관련 테스트", () => {
   test("숫자 생성 테스트", () => {
-    const generatedNumber = new ComputerNumberGenerator()
-      .getGeneratedComputerNumber;
+    const generatedNumber =
+      new ComputerNumberGenerator().getGeneratedComputerNumber();
     expect(new Set(generatedNumber).size).toEqual(3);
     generatedNumber.forEach((singleElement) => {
       expect(typeof singleElement).toEqual("number");
